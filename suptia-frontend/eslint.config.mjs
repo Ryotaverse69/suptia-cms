38b1b065646a5c068589e57b6ba822bd61e6f4c2
Ryotaverse69 <ryotaverse@gmail.com>
2025-07-21 21:16:24 +0900

    デプロイ修正版2

--- suptia-frontend/eslint.config.mjs
+++
@@ -1,3 +0,0 @@
-const nextEslint = require("next/eslint");
-
-module.exports = nextEslint;
--- suptia-frontend/src/app/articles/[slug]/page.tsx
+++ suptia-frontend/src/app/articles/[slug]/page.tsx
@@ -1,21 +1,23 @@
 import { client } from "@/lib/sanity.client";
 import { PortableText } from "@portabletext/react";
-import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 
 export async function generateStaticParams() {
   const paths = await client.fetch(`*[_type == "ingredientGuide"]{"slug": slug.current}`);
   return paths.map((p: { slug: any; }) => ({ slug: p.slug }));
 }
 
-type PageProps = InferGetStaticPropsType<typeof getStaticProps>
+interface Article {
+  title: string;
+  body: any;
+}
 
-export default async function ArticlePage({ params }: PageProps) {
+export default async function ArticlePage({ params }: any) {
   const query = `*[_type == "ingredientGuide" && slug.current == $slug][0]{
     title,
     body
   }`;
 
-  const article = await client.fetch(query, { slug: params.slug });
+  const article: Article = await client.fetch(query, { slug: params.slug });
 
   return (
     <div>
@@ -23,19 +25,4 @@ export default async function ArticlePage({ params }: PageProps) {
       <PortableText value={article.body} />
     </div>
   );
-}
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const { slug = "" } = context.params as { slug: string };
-  const query = `*[_type == "ingredientGuide" && slug.current == $slug][0]{
-    title,
-    body
-  }`;
-  const article = await client.fetch(query, { slug });
-  return {
-    props: {
-      article,
-      params: { slug }
-    }
-  }
 }
