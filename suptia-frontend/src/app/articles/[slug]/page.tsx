78c6cfdc897c8236f1bceca7e29ec9f08655f856
Ryotaverse69 <ryotaverse@gmail.com>
2025-07-21 20:50:34 +0900

    デプロイ修正版

--- suptia-frontend/src/app/articles/[slug]/page.tsx
+++ suptia-frontend/src/app/articles/[slug]/page.tsx
@@ -1,12 +1,15 @@
 import { client } from "@/lib/sanity.client";
 import { PortableText } from "@portabletext/react";
+import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 
 export async function generateStaticParams() {
   const paths = await client.fetch(`*[_type == "ingredientGuide"]{"slug": slug.current}`);
   return paths.map((p: { slug: any; }) => ({ slug: p.slug }));
 }
 
-export default async function ArticlePage({ params }: { params: { slug: string } }) {
+type PageProps = InferGetStaticPropsType<typeof getStaticProps>
+
+export default async function ArticlePage({ params }: PageProps) {
   const query = `*[_type == "ingredientGuide" && slug.current == $slug][0]{
     title,
     body
@@ -20,4 +23,19 @@ export default async function ArticlePage({ params }: { params: { slug: string }
       <PortableText value={article.body} />
     </div>
   );
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { slug = "" } = context.params as { slug: string };
+  const query = `*[_type == "ingredientGuide" && slug.current == $slug][0]{
+    title,
+    body
+  }`;
+  const article = await client.fetch(query, { slug });
+  return {
+    props: {
+      article,
+      params: { slug }
+    }
+  }
 }
